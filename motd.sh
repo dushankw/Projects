af40d72eeda068d65d3a679c15b85a871d27f6e8
dushankw <dushan.android@gmail.com>
2013-11-20 15:36:27 +1100

    Updates

--- motd.sh
+++ motd.sh
@@ -3,12 +3,5 @@
 # Written for Debian/Ubuntu
 # dushankw - http://dushan.it
 
-# This script changes the SSH MOTD to my liking
-# Install "update-motd" with "sudo apt-get install update-motd"
-# Delete the contents of /etc/update-motd.d/ then copy this file there
-# Set permissions "chmod 700 <filename>"
-# Wait a few mins for the update-motd cronjob to pick it up
-# Log out and then SSH in again, you will see the new MOTD
-
 echo "\nWelcome to `lsb_release -sd` (`uname -rm`)"
 echo "`hostname` up for `uptime | awk '{print $3 " " $4 }'` load average `cat /proc/loadavg`\n"
