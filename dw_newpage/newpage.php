7bbdbb223cf4c7584a56a82a57455eee2677655e
dushankw <dushan.android@gmail.com>
2014-06-18 18:07:56 +1000

    added function call

--- dw_newpage/newpage.php
+++ dw_newpage/newpage.php
@@ -7,22 +7,22 @@
      * If I'm called directly the die(); clause will run
      * ANYONE can POST to me, but this is only dangerous if the Wiki
      * itself is not configured well, see README for more info 
+     *
+     * NOTE: DokuWiki does not use a SQL database, so no explicit
+     * SQL Sanitising is done
      */
 
     // The site
     $wiki = "http://wiki.yoursite.com";
     
     // Get inputs
-    $pagename = $_POST['page'];
+    $pagename = htmlspecialchars($_POST['page']);
     $pattern = "/^[a-zA-Z0-9:]+$/";
 
     // Check submmited value matches regex, if so it is safe and can procede
-    if (preg_match($pattern, $pagename))
-    {
+    if (preg_match($pattern, $pagename)) {
         header("location: $wiki/doku.php?id=$pagename&do=edit");
-    }
-    else
-    {
+    } else {
         die("<a href='$wiki'>Try Again</a>");
     }
 
